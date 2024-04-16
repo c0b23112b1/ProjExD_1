739eedf1ce7697323f980990eb0b8c1d7ba6da0b
Nita <C0B23112b1@edu.teu.ac.jp>
2024-04-16 17:25:07 +0900

    練習8

--- pygame_image.py
+++ pygame_image.py
@@ -16,17 +16,39 @@ def main():
 
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img,True,False)
-    kk_img = pg.transform.rotozoom(kk_img,10,1.0)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 300,200
+
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-            x = tmr%3200
+
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            #print("上押された")
+            kk_rct.move_ip((0,-1))
+
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip((0,1))
+
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip((1,0))
+
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip((-1,0))
+        
+
+        x = tmr%3200
+
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_img2,[-x+1600,0])
         screen.blit(bg_img, [-x+3200, 0])
         screen.blit(bg_img2,[-x+4800,0])
-        screen.blit(kk_img,[300,200])
+
+        screen.blit(kk_img,kk_rct) #座標固定のときは[]
+
+
 
         pg.display.update()
         tmr += 1        
